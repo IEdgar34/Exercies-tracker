5e49a286e06a4d0831190d06a337165655258a5a
Edgar ed <edo.jan.94@bk.ru>
2025-01-31 14:40:30 +0000

    first

--- index.js
+++ index.js
@@ -2,15 +2,49 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("mongobd conected"))
 
 
 
+const userSchema =  new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+    
+  },
+  
+})
+//модель по схеме
+const User = mongoose.model("user",userSchema)
+
+
+    app.post('/api/users',(req,res) => {
+      let pers = new User({name:req.body.username})
+       pers.save().then(response => {
+        res.json({
+          username: response.name,
+          _id: response._id
+        })
+       }).catch(err => console.log(err))
+      
+    })
+    app.get("/api/users" ,(req,res) => {
+     
+      User.find({}).then(resp => console.log(resp.map((item) => {
+        return {
+          name: item.name,
+          _id: item._id
+        }
+      })))
+    })
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
